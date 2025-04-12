8b1cd283bf5cde3441bcd1a30be53325beca0b7b
慶 竹越 <K024G0107@g.neec.ac.jp>
2025-04-12 15:08:17 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <Novice.h>
+#include <cmath>
 
-const char kWindowTitle[] = "LE2A_09_タケコシケイ_MT3";
+const char kWindowTitle[] = "LE2A_09_タケコシケイ_MT3_00_05_確認課題";
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+Matrix4x4 Multiply(Matrix4x4 matrix1, Matrix4x4 matrix2);
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label);
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,6 +29,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	//=================================================
+	// Vector3の変数
+	//=================================================
+	Vector3 scale = { 1.2f, 0.79f, -2.1f };
+	Vector3 rotate = { 0.4f, 1.43f, -0.8f };
+	Vector3 translate = { 2.7f, -4.15f, 1.57f };
+
+	//=================================================
+	// Matrix4x4の変数
+	//=================================================
+	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -33,6 +63,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
+		//=================================================
+		// screenPrintf
+		//=================================================
+		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
+
 		///
 		/// ↑描画処理ここまで
 		///
@@ -49,4 +84,100 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの終了
 	Novice::Finalize();
 	return 0;
+}
+
+/// <summary>
+/// アフィン行列を作成
+/// </summary>
+/// <param name="scale">拡縮</param>
+/// <param name="rotate">回転</param>
+/// <param name="translate">移動</param>
+/// <returns>4x4アフィン行列</returns>
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+	Matrix4x4 affineMatrix;
+
+	// 回転行列（X軸回転）
+	Matrix4x4 rotateX = {
+		1.0f,  0.0f, 0.0f, 0.0f,
+		0.0f, std::cos(rotate.x), std::sin(rotate.x), 0.0f,
+		0.0f, std::sin(-rotate.x), std::cos(rotate.x), 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	};
+
+	// 回転行列（Y軸回転）
+	Matrix4x4 rotateY = {
+		std::cos(rotate.y), 0.0f, std::sin(-rotate.y), 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		std::sin(rotate.y), 0.0f, std::cos(rotate.y), 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	};
+
+	// 回転行列（Z軸回転）
+	Matrix4x4 rotateZ = {
+		std::cos(rotate.z), std::sin(rotate.z), 0.0f, 0.0f,
+		std::sin(-rotate.z), std::cos(rotate.z), 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	};
+
+	// 回転行列を合成
+	Matrix4x4 rotationMatrix = Multiply(rotateX, Multiply(rotateY, rotateZ));
+
+	// スケーリング行列
+	Matrix4x4 scaleMatrix = {
+		scale.x, 0.0f, 0.0f, 0.0f,
+		0.0f, scale.y, 0.0f, 0.0f,
+		0.0f, 0.0f, scale.z, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	};
+
+	// 平行移動行列
+	Matrix4x4 translateMatrix = {
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		translate.x, translate.y, translate.z, 1.0f
+	};
+
+	affineMatrix = Multiply(scaleMatrix, Multiply(rotationMatrix, translateMatrix));
+
+	return affineMatrix;
+}
+
+/// <summary>
+/// 4x4行列の乗算
+/// </summary>
+/// <param name="matrix1">4x4行列 1</param>
+/// <param name="matrix2">4x4行列 2</param>
+/// <returns>4x4行列の乗算</returns>
+Matrix4x4 Multiply(Matrix4x4 matrix1, Matrix4x4 matrix2) {
+	Matrix4x4 multiplyMatrix;
+	float multiply[4];  // 4x4行列に合わせてサイズを変更
+
+	for (int row = 0; row < 4; row++) {
+		for (int column = 0; column < 4; column++) {
+			for (int i = 0; i < 4; i++) {
+				multiply[i] = matrix1.m[row][i] * matrix2.m[i][column];
+			}
+			multiplyMatrix.m[row][column] = multiply[0] + multiply[1] + multiply[2] + multiply[3];
+		}
+	}
+
+	return multiplyMatrix;
+}
+
+/// <summary>
+/// 4x4行列の値をっ表示
+/// </summary>
+/// <param name="x">x座標</param>
+/// <param name="y">y座標</param>
+/// <param name="matrix">表示する行列</param>
+/// <param name="label">ラベル</param>
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + 24 + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+	Novice::ScreenPrintf(x + 24, y, "%s", label);
 }
